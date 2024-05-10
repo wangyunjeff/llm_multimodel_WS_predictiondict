f0e3b56ef9e5d7362bb001201910892c63f0ca85
wangyun <wangyunjeff@qq.com>
2024-05-10 20:29:57 +0800

    change

---
+++ model/lstm_model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+# LSTM model definition
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_layer_size, output_size):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+    def forward(self, input_seq):
+        lstm_out, _ = self.lstm(input_seq)
+        predictions = self.linear(lstm_out[:, -1, :])
+        return predictions
--- train_lstm1.py
+++ train_lstm1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import torch
 import torch.nn as nn
+from model.lstm_model import LSTM
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 
@@ -18,20 +19,6 @@ test_dataset = WindTurbineDataset(path, seq_length, columns=columns, split='test
 # DataLoader
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64)
-
-
-# LSTM model definition
-class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_layer_size, output_size):
-        super().__init__()
-        self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
-        self.linear = nn.Linear(hidden_layer_size, output_size)
-
-    def forward(self, input_seq):
-        lstm_out, _ = self.lstm(input_seq)
-        predictions = self.linear(lstm_out[:, -1, :])
-        return predictions
-
 
 # Model instantiation
 model = LSTM(input_size=1, hidden_layer_size=100, output_size=1)
@@ -92,4 +79,4 @@ for epoch in range(epochs):
         plt.xlabel('Time')
         plt.ylabel('Target Variable')
         plt.legend()
-        plt.show()
+        plt.savefig(f'{epoch}.png')
